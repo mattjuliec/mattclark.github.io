3d93882ae9adeb5b758a8803110d8a945f503c45
Matthew A Clark <mattclark@galois.com>
2019-12-13 08:13:31 -0500

    Reorganize based on example

---
+++ src/Main.elm
@@ -0,0 +1,18 @@
+module HomePage exposing (main)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+view model =
+    div [ class "jumbotron" ]
+        [ h1 [] [ text "Welcome to Matt Clark's Personal Page!" ]
+        , p []
+            [ text "This is  a work in Progress using "
+            , strong [] [ text "Elm" ]
+            ]
+        ]
+
+
+main =
+    view "dummy model"
