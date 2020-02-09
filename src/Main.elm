d55c6481906a9e6998391e3c5c175c748ac78b30
Matthew Clark <mattclark@matthews-mbp.lan>
2020-02-08 20:02:43 -0500

    Another test

--- src/Main.elm
+++ src/Main.elm
@@ -6,7 +6,7 @@ import Html.Attributes exposing (..)
 
 view model =
     div [ class "jumbotron" ]
-        [ h1 [] [ text "Welcome to Matt Clark's Personal Page!" ]
+        [ h1 [] [ text "Welcome to Matt Clark's Page for the People" ]
         , p []
             [ text "This is  a work in Progress using "
             , strong [] [ text "Elmish Land" ]
